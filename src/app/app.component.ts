be7011300b27ba0d64b93e4be0a61baf6b74fa76
Alex Felix <85413825+afsilva3021@users.noreply.github.com>
2024-05-09 10:37:13 -0300

    Concluida atividade componentes angular
    Foi criado 2 componentes navbar e componente body com galeria de imagens conforme solicitado

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BodyComponent } from './components/body/body.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,NavbarComponent, BodyComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+  
+})
+
+
+export class AppComponent {
+  title = 'atividade_11';
+}
---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './body.component.html',
+  styleUrl: './body.component.css'
+})
+
+
+export class BodyComponent {
+  title = "Galeria";
+
+}
